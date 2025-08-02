d6542624687703e6aacc8b326f777c577b060af0
harry2690 <harry2690@users.noreply.github.com>
2025-08-03 00:34:53 +0800

    refactor: adopt BaseChatModel abstraction

--- tradingagents/agents/utils/agent_utils.py
+++ tradingagents/agents/utils/agent_utils.py
@@ -9,7 +9,6 @@ import functools
 import pandas as pd
 import os
 from dateutil.relativedelta import relativedelta
-from langchain_openai import ChatOpenAI
 import tradingagents.dataflows.interface as interface
 from tradingagents.default_config import DEFAULT_CONFIG
 from langchain_core.messages import HumanMessage
